4f7f7d23e0ebdf6a4bb72c85f9ed732f8570adcc
Pal patel <115624654+PalProdution@users.noreply.github.com>
2024-07-25 12:53:23 +0530

    Create piece.py

---
+++ piece.py
@@ -0,0 +1,31 @@
+import os
+
+class Piece:
+
+    def __init__(self, name, color, value, texture=None, texture_rect=None):
+        self.name = name
+        self.color = color
+        value_sign = 1 if color == 'white' else -1
+        self.value = value * value_sign
+        self.moves = []
+        self.moved = False
+        self.texture = texture
+        self.set_texture()
+        self.texture_rect = texture_rect
+
+    def set_texture(self, size=80):
+        self.texture = os.path.join(
+            f'assets/images/imgs-{size}px/{self.color}_{self.name}.png')
+
+    def add_move(self, move):
+        self.moves.append(move)
+
+    def clear_moves(self):
+        self.moves = []
+
+class Pawn(Piece):
+
+    def __init__(self, color):
+        self.dir = -1 if color == 'white' else 1
+        self.en_passant = False
+        super().__init__('pawn', color, 1.0)
