c9ec7f51cd593a8e221f8b42601cd468531c409a
Pal patel <115624654+PalProdution@users.noreply.github.com>
2024-07-26 17:42:29 +0530

    Update game.py

--- game.py
+++ game.py
@@ -48,3 +48,47 @@ class Game:
                     lbl_pos = (col * SQSIZE + SQSIZE - 20, HEIGHT - 20)
                     # blit
                     surface.blit(lbl, lbl_pos)
+                       def show_pieces(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                # piece ?
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].piece
+                    
+                    # all pieces except dragger piece
+                    if piece is not self.dragger.piece:
+                        piece.set_texture(size=80)
+                        img = pygame.image.load(piece.texture)
+                        img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
+                        piece.texture_rect = img.get_rect(center=img_center)
+                        surface.blit(img, piece.texture_rect)
+
+    def show_moves(self, surface):
+        theme = self.config.theme
+
+        if self.dragger.dragging:
+            piece = self.dragger.piece
+
+            # loop all valid moves
+            for move in piece.moves:
+                # color
+                color = theme.moves.light if (move.final.row + move.final.col) % 2 == 0 else theme.moves.dark
+                # rect
+                rect = (move.final.col * SQSIZE, move.final.row * SQSIZE, SQSIZE, SQSIZE)
+                # blit
+                pygame.draw.rect(surface, color, rect)
+
+    def show_last_move(self, surface):
+        theme = self.config.theme
+
+        if self.board.last_move:
+            initial = self.board.last_move.initial
+            final = self.board.last_move.final
+
+            for pos in [initial, final]:
+                # color
+                color = theme.trace.light if (pos.row + pos.col) % 2 == 0 else theme.trace.dark
+                # rect
+                rect = (pos.col * SQSIZE, pos.row * SQSIZE, SQSIZE, SQSIZE)
+                # blit
+                pygame.draw.rect(surface, color, rect)
