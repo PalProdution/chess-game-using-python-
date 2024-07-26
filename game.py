2ea11eadc2181ab08c5fa36c336eb2df363880ba
Pal patel <115624654+PalProdution@users.noreply.github.com>
2024-07-26 16:44:00 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,50 @@
+import pygame
+
+from const import *
+from board import Board
+from dragger import Dragger
+from config import Config
+from square import Square
+
+class Game:
+
+    def __init__(self):
+        self.next_player = 'white'
+        self.hovered_sqr = None
+        self.board = Board()
+        self.dragger = Dragger()
+        self.config = Config()
+
+    # blit methods
+
+    def show_bg(self, surface):
+        theme = self.config.theme
+        
+        for row in range(ROWS):
+            for col in range(COLS):
+                # color
+                color = theme.bg.light if (row + col) % 2 == 0 else theme.bg.dark
+                # rect
+                rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
+                # blit
+                pygame.draw.rect(surface, color, rect)
+
+                # row coordinates
+                if col == 0:
+                    # color
+                    color = theme.bg.dark if row % 2 == 0 else theme.bg.light
+                    # label
+                    lbl = self.config.font.render(str(ROWS-row), 1, color)
+                    lbl_pos = (5, 5 + row * SQSIZE)
+                    # blit
+                    surface.blit(lbl, lbl_pos)
+
+                # col coordinates
+                if row == 7:
+                    # color
+                    color = theme.bg.dark if (row + col) % 2 == 0 else theme.bg.light
+                    # label
+                    lbl = self.config.font.render(Square.get_alphacol(col), 1, color)
+                    lbl_pos = (col * SQSIZE + SQSIZE - 20, HEIGHT - 20)
+                    # blit
+                    surface.blit(lbl, lbl_pos)
